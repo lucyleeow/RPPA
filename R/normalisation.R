ff42142eb25ca7dfb87c7ea0533fd205181ae376
lucyleeow <jliu176@gmail.com>
2018-10-18 15:56:31 +1100

    Add normalisation file

---
+++ R/normalisation.R
@@ -0,0 +1,39 @@
+#################################################################
+# Functions to normalise RPPA data using various normalisation
+# methods
+#
+# Author: Lucy Liu
+#################################################################
+
+
+##########################################################
+# Perform median normalisation
+#
+# This method assumes that the amount of all proteins measured in the 
+# experiment reflect the total protein amount of one sample. Thus, the 
+# median AB of a sample estimates sample loading. 
+#
+# Divide all raw values by the median value of all AB signals of that
+# sample is used to normalise the raw intensity values for each AB. 
+# May be biased when the number of ABs is <100
+##########################################################
+
+
+norm_med <- function(
+  df    # df in tidy format with NO technical replicates
+){
+  
+  # check inputs
+  assertthat::assert_that()
+  
+  norm_df <- df %>%
+    group_by(X1) %>%
+    mutate(med = median(RFI)) %>%
+    ungroup() %>%
+    mutate(MN = RFI/med) %>%
+    select(-med)
+  
+  
+  return(norm_df)
+  
+}
